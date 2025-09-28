7f01c8d765fbc87e1470437bc4e8376273361fa9
john-otienoh <johnteclaire@gmail.com>
2025-09-28 11:47:43 +0300

    Create project directory

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Number-Guessing-Game
 
+[![wakatime](https://wakatime.com/badge/user/6d7c48ee-9185-4251-a234-e029aa6b148d/project/a3351af6-eae0-4af3-a6ff-d58cba927230.svg)](https://wakatime.com/badge/user/6d7c48ee-9185-4251-a234-e029aa6b148d/project/a3351af6-eae0-4af3-a6ff-d58cba927230)
+
 A simple number guessing game where the computer randomly selects a number and the user has to guess it. The user will be given a limited number of chances to guess the number. If the user guesses the number correctly, the game will end, and the user will win. Otherwise, the game will continue until the user runs out of chances.
 
 ```text
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"main/utils"
+	"strconv"
+	"time"
 )
 
 func main() {
@@ -15,7 +17,8 @@ func main() {
 	1. Easy (10 chances)
 	2. Medium (5 chances)
 	3. Hard (3 chances)`)
-	fmt.Printf("Random number is %d\n", utils.GenerateRandomNumber())
+	random_number := utils.GenerateRandomNumber()
+	fmt.Println(random_number)
 
 	var user_choice int
 	var difficulty = make(map[string]int, 0)
@@ -25,10 +28,61 @@ func main() {
 	fmt.Println("Enter the first number.")
 	fmt.Scanln(&user_choice)
 
+	chances := 0
 	if user_choice <= 3 && user_choice >= 1 {
 		fmt.Printf("User number is %d\n", user_choice)
+		switch user_choice {
+		case 1:
+			chances = difficulty["Easy"]
+			fmt.Printf("Great! You have selected the Easy difficulty level.\nYou have %d chances\n", chances)
+			fmt.Println("Let's start the game!")
+		case 2:
+			chances = difficulty["Medium"]
+			fmt.Printf("Great! You have selected the Medium difficulty level.\nYou have %d chances\n", chances)
+			fmt.Println("Let's start the game!")
+		default:
+			chances = difficulty["Hard"]
+			fmt.Printf("Great! You have selected the Hard difficulty level.\nYou have %d chances\n", chances)
+			fmt.Println("Let's start the game!")
+		}
 	} else {
 		fmt.Println("Invalid Choice")
 	}
+	attempts := 0
+	start_time := time.Now()
 
+	for chances > 0 {
+		var guess string
+		fmt.Println("Enter your guess:")
+		fmt.Scanln(&guess)
+
+		if number, err := strconv.Atoi(guess); err == nil {
+			if number < 101 && number > 0 {
+				fmt.Printf("You have guessed %d\n", number)
+				// break
+			} else {
+				fmt.Println("Number guess should be between 1 to 100")
+				continue
+			}
+		} else {
+			fmt.Println("Invalid input")
+			continue
+		}
+		attempts++
+		number, _ := strconv.Atoi(guess)
+		if number == random_number {
+			elapsed_time := time.Since(start_time)
+			fmt.Printf("🎉 Congratulations! You guessed the number in %d attempts\n", attempts)
+			fmt.Println("Time taken: ", elapsed_time)
+			break
+		} else if random_number < number {
+			fmt.Printf("Incorrect! The number is less than %d\n", number)
+		} else {
+			fmt.Printf("Incorrect! The number is greater than %d\n", number)
+		}
+		chances--
+		if chances == 0 {
+			fmt.Printf("😞 You ran out of chances. The correct number was %d\n", random_number)
+		}
+	}
 }
